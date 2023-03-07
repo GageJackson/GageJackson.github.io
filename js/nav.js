5fc8e3991846a2f78e218de790fbffc71c2e3892
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-07 15:14:27 -0600

    created a dark mode for the body, working to get it for all set elements next

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,8 @@
 const primaryNav = document.querySelector('.primary-nav');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const darkBody = document.querySelector('body');
+const darkMode = localStorage.getItem("theme");
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute("data-visible");
@@ -13,3 +16,26 @@ navToggle.addEventListener('click', () => {
     }
     console.log(visibility);
 })
+
+darkModeToggle.addEventListener('click', () => {
+    const darkModeStatus = document.body.getAttribute('class');
+    let darkMode;
+    if( darkModeStatus === 'light-mode'){
+        document.body.setAttribute('class','dark-mode')
+        darkMode = true;
+    } else if (darkModeStatus === 'dark-mode'){
+        document.body.setAttribute('class','light-mode')
+        darkMode = false;
+    }
+    localStorage.setItem("theme",darkMode);
+});
+
+function doDarkMode(){
+    if( darkMode === 'false'){
+        document.body.setAttribute('class','light-mode')
+    } else if (darkMode === 'true'){
+        document.body.setAttribute('class','dark-mode')
+    }
+}
+
+doDarkMode();
