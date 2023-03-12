7a2366a80071a398db6836c326352a04453d1aef
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-12 03:34:58 -0500

    interesting animation. want it to grow out. going to bed

--- js/gooey-btn.js
+++ js/gooey-btn.js
@@ -3,13 +3,15 @@ const gooParticles = document.getElementsByClassName("goo");
 
 gooButton.addEventListener("pointerenter", (event) => {
     for (let i = 0; i < gooParticles.length; i++) {
-        let randomNum1 = (Math.floor((Math.random() * 20) + 100));
-        let randomNum2 = (Math.floor((Math.random() * 20) + 100));
+        let randomNum1 = (Math.floor((Math.random() * 20) + 50));
+        let randomNum2 = (Math.floor((Math.random() * 20) + 50));
         let randomAngle = Math.random() * Math.PI * 2;
         let x = Math.cos(randomAngle) * randomNum1;
         let y = Math.sin(randomAngle) * randomNum2;
         gooParticles[i].style.left = `calc(50% + ${x}px)`;
         gooParticles[i].style.top = `calc(50% + ${y}px)`;
+        gooParticles[i].classList.add("goo-active");
+
     }
 });
 
@@ -20,6 +22,8 @@ gooButton.addEventListener("pointerleave", (event) =>{
         gooParticles[i].style.removeProperty("right");
         gooParticles[i].style.removeProperty("bottom");
         gooParticles[i].style.removeProperty("left");
+        gooParticles[i].classList.remove("goo-active");
+
     }
     });
 
