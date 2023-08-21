456b49a6d91c8f4876a2bab8b67206b360ad1a33
GageJackson <wm.gagejackson@gmail.com>
2023-08-21 16:55:31 -0500

    slight tweaks

--- js/bg-animation.js
+++ js/bg-animation.js
@@ -1,119 +1,110 @@
-// settings
-let updateRate = 1000 / 60;
+// Settings
+const updateRate = 1000 / 60;
 let lastTime = 0;
 let elapsedTime = 0;
 
-// canvas variables
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext('2d');
+// Canvas setup
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+resizeCanvas();
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Calculate canvas dimensions
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
 
-let cw = canvas.width;
-let ch = canvas.height;
-let ca = cw * ch;
-
-// fps
+// FPS calculation
 let fps = 0;
 let accumulatedTime = 0;
 let accumulatedFrames = 0;
 
-let numCircles = 30;
-let circles = [];
-let velocity = 1;
-// let size = ((ca * .15) / (numCircles * numCircles));
-let size = Math.min(cw, ch) * 6 / numCircles;
+// Circle properties
+const numCircles = 30;
+const circles = [];
+const velocity = 1;
+const size = Math.min(canvas.width, canvas.height) * 6 / numCircles;
 
-// console.log(size);
+// Initialize circles
+function initializeCircles() {
+    for (let i = 0; i < numCircles; i++) {
+        const randSize = (Math.random() * size) + 10;
 
+        const gradient = ctx.createRadialGradient(
+            canvas.width / 2, 0, 0, canvas.width / 2, 0, Math.max(canvas.height, canvas.width)
+        );
+        gradient.addColorStop(0, "hsl(43,100%,50%)");
+        gradient.addColorStop(0.5, "hsl(28,93%,48%)");
+        gradient.addColorStop(1, "hsl(16,86%,46%)");
+
+        circles.push({
+            size: randSize,
+            gradient: gradient,
+            x: (Math.random() - 0.5) * canvas.width + canvas.width / 2,
+            y: (Math.random() - 0.5) * canvas.height + canvas.height / 2,
+            vx: (Math.random() - 0.5) * velocity * 1,
+            vy: (Math.random() - 0.5) * velocity * 1
+        });
+    }
+}
+
+// Main loop
 function main(timestamp) {
+    resizeCanvas();
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    cw = canvas.width;
-    ch = canvas.height;
-
-    // accumulating time and frames for FPS
     accumulatedTime += timestamp - lastTime;
     accumulatedFrames++;
 
-    // keeping track of passed time
     elapsedTime += timestamp - lastTime;
     lastTime = timestamp;
 
-    // calculating the fps
     if (accumulatedTime >= 1000) {
         fps = accumulatedFrames;
         accumulatedFrames = 0;
         accumulatedTime -= 1000;
     }
 
-    // logic update - if machine is slow then do multiple steps to catch up
     while (elapsedTime >= updateRate) {
         update();
         elapsedTime -= updateRate;
     }
 
-    // render update - render every possible frame
     render();
-
-    // repeat loop
     requestAnimationFrame(main);
 }
 
+// Update circle positions
 function update() {
-    for (let i = 0; i < circles.length; i++) {
-        let circle = circles[i];
+    for (const circle of circles) {
         circle.x += circle.vx;
         circle.y += circle.vy;
 
-        // horizontal boundary
-        if (circle.x <= circle.size || circle.x >= cw - circle.size) {
+        // Horizontal Boundaries
+        if (circle.x <= circle.size || circle.x >= canvas.width - circle.size) {
             circle.vx *= -1;
-            circle.x = circle.x <= circle.size ? circle.size : cw - circle.size;
+            circle.x = circle.x <= circle.size ? circle.size : canvas.width - circle.size;
         }
-        // vertical boundary
-        if (circle.y <= circle.size || circle.y >= ch - circle.size) {
+
+        //Vertical Boundaries
+        if (circle.y <= circle.size || circle.y >= canvas.height - circle.size) {
             circle.vy *= -1;
-            circle.y = circle.y <= circle.size ? circle.size : ch - circle.size;
+            circle.y = circle.y <= circle.size ? circle.size : canvas.height - circle.size;
         }
     }
 }
 
+// Render circles
 function render() {
-    ctx.clearRect(0, 0, cw, ch);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // circles
-    for (let i = 0; i < circles.length; i++) {
-        ctx.fillStyle = circles[i].gradient;
+    for (const circle of circles) {
+        ctx.fillStyle = circle.gradient;
         ctx.beginPath();
-        const radius = circles[i].size;
-
-        ctx.arc(circles[i].x, circles[i].y, radius, 0, 2 * Math.PI);
+        ctx.arc(circle.x, circle.y, circle.size, 0, 2 * Math.PI);
         ctx.fill();
     }
 }
 
-
-// create the circles
-for (let i = 0; i < numCircles; i++) {
-    let randSize = (Math.random() * size) + 10;
-
-    // Create a radial gradient
-    const gradient = ctx.createRadialGradient((cw/2), 0, 0, (cw/2), 0, Math.max(ch,cw));
-    gradient.addColorStop(0, "hsl(43,100%,50%)");
-    gradient.addColorStop(0.5, "hsl(28,93%,48%)");
-    gradient.addColorStop(1, "hsl(16,86%,46%)");
-
-    circles.push({
-        size: randSize,
-        gradient: gradient,
-        x: (Math.random() - 0.5) * cw + cw/2,
-        y: (Math.random() - 0.5) * ch + ch/2,
-        vx: (Math.random() - 0.5) * velocity * 1,
-        vy: (Math.random() - 0.5) * velocity * 1
-    });
-}
-
+// Initialize and start
+initializeCircles();
 requestAnimationFrame(main);
